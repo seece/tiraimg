be477ec4a4c06fb358287123cb718dc6ae9df261
Pekka Väänänen <pekka.vaananen@iki.fi>
2013-05-16 23:21:39 +0300

    add block multiply test

--- src/test/blockTest.c
+++ src/test/blockTest.c
@@ -1,20 +1,30 @@
 
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdbool.h>
+#include <limits.h>
 #include "eks_math.h"
 #include "CuTest.h"
 #include "block.h"
 #include "test_data.h"
 
+static const int size = TIMG_BLOCK_SIZE;
+
+void generate_random_floatblock(struct FloatBlock* blockp, float low, float high)
+{
+	for (int y=0;y<size;y++) {
+		for (int x=0;x<size;x++) {
+			blockp->data[y][x] = low + (float)rand()/((float)RAND_MAX/(high-low));
+		}
+	}
+}
 
 void TestByteBlockInit(CuTest* tc) 
 {
 	struct ByteBlock* blockp = new_byteblock();
-
-	int size = TIMG_BLOCK_SIZE;
 
 	for (int y=0;y<size;y++) {
 		for (int x=0;x<size;x++) {
@@ -29,8 +39,6 @@ void TestFloatBlockInit(CuTest* tc)
 {
 	struct FloatBlock* blockp = new_floatblock();
 
-	int size = TIMG_BLOCK_SIZE;
-
 	for (int y=0;y<size;y++) {
 		for (int x=0;x<size;x++) {
 			CuAssertDblEquals(tc, 0.0, blockp->data[y][x], 0.0);
@@ -42,7 +50,7 @@ void TestFloatBlockInit(CuTest* tc)
 
 void TestBlockBias(CuTest* tc) 
 {
-	int size = TIMG_BLOCK_SIZE;
+
 	struct ByteBlock input;
 	memcpy(&input, &test_input, sizeof(input));
 	struct FloatBlock output;
@@ -58,11 +66,37 @@ void TestBlockBias(CuTest* tc)
 	}
 }
 
+void TestBlockMultiply(CuTest* tc) 
+{
+	struct FloatBlock first;
+	struct FloatBlock second;
+	struct FloatBlock third;
+
+	srand(13);
+	generate_random_floatblock(&first, 0.0, 10.0);
+	srand(14);
+	generate_random_floatblock(&second, -10.0, 0.0);
+	srand(14);
+	generate_random_floatblock(&third, -10.0, 0.0);
+
+	multiply_floatblock(&first, &second);
+
+	// The third block contains the original values of the second block.
+
+	for (int y=0;y<size;y++) {
+		for (int x=0;x<size;x++) {
+			CuAssertDblEquals(tc, first.data[y][x] * third.data[y][x], 
+					second.data[y][x], 0.0001);
+		}
+	}
+}
+
 CuSuite* CuGetBlockSuite(void) 
 {
 	CuSuite* suite = CuSuiteNew();
 	SUITE_ADD_TEST(suite, TestByteBlockInit);
 	SUITE_ADD_TEST(suite, TestFloatBlockInit);
 	SUITE_ADD_TEST(suite, TestBlockBias);
+	SUITE_ADD_TEST(suite, TestBlockMultiply);
 	return suite;
 }
